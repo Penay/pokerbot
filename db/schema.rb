30fddbb48cfc8d41da85ef9e9fe09793b113483d
Denys Medynskiy <denys.medynskyi@gmail.com>
2013-05-18 08:32:43 +0300

    lol

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324102316) do
+ActiveRecord::Schema.define(:version => 20130410100643) do
+
+  create_table "decks", :force => true do |t|
+    t.text     "cards"
+    t.integer  "game_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "games", :force => true do |t|
     t.integer  "user_id"
